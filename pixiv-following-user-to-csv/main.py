ed6d8a9f737d36aa012ff2fb0e6d07fb5d1d4362
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-11 23:47:01 +0900

    feat: Init logger

--- pixiv-following-user-to-csv/main.py
+++ pixiv-following-user-to-csv/main.py
@@ -1,6 +1,12 @@
+from utils.logger import init_logger
+
+
 class AppClass:
-    def main():
-        print("This is the main function.")
+    def __init__(self):
+        self.logger = init_logger()
+
+    def main(self):
+        self.logger.info("This is the main function.")
 
 
 if __name__ == "__main__":
